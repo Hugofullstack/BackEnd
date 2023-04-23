99c605a4077f7f946929bd1d2dc94b3558b4c44c
Hugo Full <hugocatalan2@gmail.com>
2023-04-23 19:45:52 -0300

    clonbueno

---
+++ src/main/java/com/example/demo/Dto/dtoProyecto.java
@@ -0,0 +1,74 @@
+package com.example.demo.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * 
+ */
+public class dtoProyecto {
+
+    @NotBlank
+    private String nombreP;
+    @NotBlank
+    private String descripcionP;
+    @NotBlank
+    private String imagenP;
+    @NotBlank
+    private String fechaP;
+    @NotBlank
+    private String linkP;
+
+    public dtoProyecto() {
+    }
+
+    public dtoProyecto(String nombreP, String descripcionP, String imagenP, String fechaP, String linkP) {
+        this.nombreP = nombreP;
+        this.descripcionP = descripcionP;
+        this.imagenP = imagenP;
+        this.fechaP = fechaP;
+        this.linkP = linkP;
+
+    }
+
+    public String getNombreP() {
+        return nombreP;
+    }
+
+    public void setNombreP(String nombreP) {
+        this.nombreP = nombreP;
+    }
+
+    public String getDescripcionP() {
+        return descripcionP;
+    }
+
+    public void setDescripcionP(String descripcionP) {
+        this.descripcionP = descripcionP;
+    }
+
+    public String getImagenP() {
+        return imagenP;
+    }
+
+    public void setImagenP(String imagenP) {
+        this.imagenP = imagenP;
+    }
+
+    public String getFechaP() {
+        return fechaP;
+    }
+
+    public void setFechaP(String fechaP) {
+        this.fechaP = fechaP;
+    }
+
+    public String getLinkP() {
+        return linkP;
+    }
+
+    public void setLinkP(String linkP) {
+        this.linkP = linkP;
+    }
+
+}
---
+++ src/main/java/com/example/demo/Repository/IPersonaRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.Persona;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * 
+ */
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Integer> {
+
+    public Optional<Persona> findByNombre(String nombre);
+
+    public boolean existsByNombre(String nombre);
+
+}
